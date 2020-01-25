14b4da0ff3cf034d0f2b083f3a02ea3492e0fa4a
Mike17Mike <Mike17Mike@users.noreply.github.com>
2020-01-24 19:24:04 -0500

    Update main.cpp
    Set up more methods to make everything more clear. Created a general idea for moving the aero

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,35 @@
-#include "mbed.h"
+#include <mbed.h>
 #include "Servo.h"
 
 //Can use PB_0 for led 
 PwmOut led(PB_0);
 
+
+//Create Servos
 Servo servo1A(PA_0);
 Servo servo2A(PA_1);
 Servo servo3A(PA_2);
 Servo servo4A(PA_3);
  
+//Create CAN message
 CAN can1(PA_11, PA_12);
 CANMessage msg;
-int brakePercent;
-int mode = 0;
-int transmission = 0;
 
+//Limits
+int aeroMax = 100;
+int minBrakePercentage;
+int minHeight;
+int aeroOnManual;                //Aero is off with 0, Aero is on with 1
 
-void moveAero(int x){
-  x = 180 * (x/100);
-  servo1A.write(x);
-  servo2A.write(x);
-  servo3A.write(x);
-  servo4A.write(x);
+//Needed Variables 
+int brakePercent;              //Variable for brake percentage
+int mode = 0;                  //Variable for the current mode of the car
+int automaticAero = 0;         //0 for automatic active aero, 1 for manual active aero
 
-  wait_ms(10);
-}
+//Function prototypes
+void moveAero(int x);
+void setTrackCondition(int x);
+
 
 int main() {
     can1.frequency(250000);
@@ -32,44 +37,62 @@ int main() {
         if(can1.read(msg)){         //Gets a message from the can bus
             if(msg.id == 0){          //Change ID later 
                 brakePercent = msg.data[0]; //The brake pressure will be stored in msg.data[x]
-                led.write(float(brakePercent/100.0));
+                led.write(float(brakePercent/100.0)); //This is just testing to see if the code was working
             }
+
+            if(msg.id == 0){                                //Check to see if you need to use button or if aero is automatic
+                automaticAero = msg.data[2];                //The data to know if the active aero is manual or auto
+            }
+
+            if(msg.id == 0 && automaticAero == 1){          //Check button for manual aero
+                aeroOnManual = msg.data[1];                 //The button for manual aero will be stored in msg.data[x]
+            }            
         }
 
-        if(transmission == 0){       //If the car is in automatic it will run
-            switch(mode){             //Check if the mode is in wet, dry, or semi-wet
-                case 1:                   //Mode is Wet
-                    if(brakePercent <= 50){
-                    moveAero(50);
-                    }
-                    else{
-                        moveAero(brakePercent);
-                    }
-                    break;
-                case 2:                   //Mode is Semi-wet
-                    if(brakePercent <= 25){
-                        moveAero(25);
-                    }
-                    else{
-                        moveAero(brakePercent);
-                    }
-                    break;
-                case 3:                   //Mode is Dry
-                    moveAero(brakePercent);
-                    break;
+        /**
+         * Need this space to read CAN message to get mode for the car
+         * and set the track conditions
+        **/
+
+        if(automaticAero == 0){                      //If the car is in automatic it will run
+            setTrackCondition(mode);    
+            if(brakePercent < minBrakePercentage){
+                moveAero(minBrakePercentage);                //Keeps the wing at minimum height
+            }
+            else{
+               moveAero(brakePercent);                       //Keeps wing at the height at the height of the brake percentage
+            }
+            
+        }
+        else{                                                //Allows Drive to use button to put wing up and wing down
+            if(aeroOnManual == 0){
+                moveAero(minHeight);
+            }
+            else{
+                moveAero(aeroMax);
             }
         }
-        else{
-            if(can1.read(msg)){       //Gathers message from can bus
-                if(msg.id == 1){        //Checks if the message is from desired location
-                    if(msg.data[0] == 0){ //If the data is true and it is turned off
-                        moveAero(0);        //Moves the wing all the way down
-                    }
-                    else if(msg.data[0] ==1){//If the button is pressed and turned on
-                        moveAero(180);      //Moves the wing all the way up
-                    }
-                }
-            }
-        }
+    }
+}
+
+void moveAero(int x){                                        //Moves aero wing to given input
+    x = 180 * (x/100);                                       //Converts the value so it is valid for the wing
+    servo1A.write(x);
+    servo2A.write(x);
+    servo3A.write(x);
+    servo4A.write(x);
+
+    wait_ms(10);
+}
+
+void setTrackCondition(int x){                                //Sets track condition so you can change limitations 
+    if(x == 1){                   //Wet mode
+        minBrakePercentage = 50;
+    }
+    else if(x == 2){              //Semi-wet mode
+        minBrakePercentage = 30;
+    }
+    else if(x == 3){              //Dry mode
+        minBrakePercentage = 0;
     }
 }
